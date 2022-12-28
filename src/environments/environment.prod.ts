40a082aaf507f08683f2b6f1b052bded3121d1f7
bayriradha <102705199+bayriradha@users.noreply.github.com>
2022-12-28 14:36:53 +0530

    Update environment.prod.ts
    Load balancer URL updated for connectivity

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://lms-api.digital-lync.com'
+  apiUrl: 'a118e524eb2ad42d1a676f42b8d117eb-1517106927.ap-south-1.elb.amazonaws.com'
 };
